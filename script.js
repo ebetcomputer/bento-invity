ae05c59e4b19ce7dc816aa1ba87bb740981258d0
ebetcomputer <53324990+ebetcomputer@users.noreply.github.com>
2026-02-21 01:21:27 +0700

    Update tema Autumn Germany dan link WhatsApp

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.querySelectorAll('.btn-outline-secondary').forEach(button => {
-    button.addEventListener('click', function() {
+//document.querySelectorAll('.btn-outline-secondary').forEach(button => {
+    //button.addEventListener('click', function() {
         // Hapus kelas aktif dari tombol lain
-        document.querySelectorAll('.btn-sage').forEach(b => {
-            b.classList.replace('btn-sage', 'btn-outline-secondary');
-            b.classList.remove('text-white');
-        });
+       // document.querySelectorAll('.btn-sage').forEach(b => {
+           // b.classList.replace('btn-sage', 'btn-outline-secondary');
+           // b.classList.remove('text-white');
+       // });
         
         // Tambahkan kelas aktif ke yang diklik
-        this.classList.replace('btn-outline-secondary', 'btn-sage');
-        this.classList.add('text-white');
-    });
-});
+       // this.classList.replace('btn-outline-secondary', 'btn-sage');
+       // this.classList.add('text-white');
+    //});
+//});
